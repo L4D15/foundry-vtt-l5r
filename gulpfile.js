09af5d210c3183881b40477d84dde460d061afb0
Ladislao Lainez <thedragonladis@gmail.com>
2020-08-13 11:54:43 +0200

    Add new gulp command to copy builds to data folder.

--- gulpfile.js
+++ gulpfile.js
@@ -302,6 +302,65 @@ async function linkUserData() {
 	}
 }
 
+/********************/
+/*		COPY		*/
+/********************/
+
+/**
+ * Copy build to User Data folder
+ */
+async function copyUserData() {
+	const name = path.basename(path.resolve('.'));
+	const config = fs.readJSONSync('foundryconfig.json');
+
+	let destDir;
+	try {
+		if (
+			fs.existsSync(path.resolve('.', 'dist', 'module.json')) ||
+			fs.existsSync(path.resolve('.', 'src', 'module.json'))
+		) {
+			destDir = 'modules';
+		} else if (
+			fs.existsSync(path.resolve('.', 'dist', 'system.json')) ||
+			fs.existsSync(path.resolve('.', 'src', 'system.json'))
+		) {
+			destDir = 'systems';
+		} else {
+			throw Error(
+				`Could not find ${chalk.blueBright(
+					'module.json'
+				)} or ${chalk.blueBright('system.json')}`
+			);
+		}
+
+		let linkDir;
+		if (config.dataPath) {
+			if (!fs.existsSync(path.join(config.dataPath, 'Data')))
+				throw Error('User Data path invalid, no Data directory found');
+
+			linkDir = path.join(config.dataPath, 'Data', destDir, name);
+		} else {
+			throw Error('No User Data path defined in foundryconfig.json');
+		}
+
+		if (argv.clean || argv.c) {
+			console.log(
+				chalk.yellow(`Removing build in ${chalk.blueBright(linkDir)}`)
+			);
+
+			await fs.remove(linkDir);
+		} else if (!fs.existsSync(linkDir)) {
+			console.log(
+				chalk.green(`Copying build to ${chalk.blueBright(linkDir)}`)
+			);
+			await fs.copy(path.resolve('./dist'), linkDir);
+		}
+		return Promise.resolve();
+	} catch (err) {
+		Promise.reject(err);
+	}
+}
+
 /*********************/
 /*		PACKAGE		 */
 /*********************/
@@ -493,6 +552,7 @@ exports.build = gulp.series(clean, execBuild);
 exports.watch = buildWatch;
 exports.clean = clean;
 exports.link = linkUserData;
+exports.copy = copyUserData;
 exports.package = packageBuild;
 exports.update = updateManifest;
 exports.publish = gulp.series(
